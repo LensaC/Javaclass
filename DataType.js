ad1d70331c7588a4003cdd15bad8db5905ec4e7d
LensaC <lensabez@outlook.com>
2023-06-16 16:02:09 -0400

    Data Type conversion for the data type class

--- DataType.js
+++ DataType.js
@@ -1,7 +1,7 @@
 let name = "Ilya";
-console.log(`hello ${1}`);
-console.log(`hello ${"name"}`);
-console.log(`hello ${name}`);
+console.log(`hello ${1}`); // hello 1
+console.log(`hello ${"name"}`); //hello name
+console.log(`hello ${name}`); //hello Ilya
 
 // Undefined Conversion 
 
@@ -12,60 +12,59 @@ let b = true
 let s = "Lensa"
 let sy = Symbol
 let N = null
-let Na = NaN
 
 // X value Conversion 
 
-console.log(typeof(x))
-console.log(Boolean(x))
-console.log(Number(x))
-console.log(String(x))
+console.log(typeof(x)) // undefined
+console.log(Boolean(x)) // false
+console.log(Number(x)) //NaN
+console.log(String(x)) // undefined
 
 
 // n value Conversion 
 
-console.log(typeof(n))
-console.log(Boolean(n))
-console.log(String(n))
+console.log(typeof(n)) // number
+console.log(Boolean(n)) // true
+console.log(String(n)) // 123
 
 
 // bg value Conversion  
 
-console.log(typeof(bg))
-console.log(Boolean(bg))
-console.log(Number(bg))
-console.log(String(bg))
+console.log(typeof(bg)) // bigint
+console.log(Boolean(bg)) // true
+console.log(Number(bg)) // 9893893898398398000
+console.log(String(bg)) // 9893893898398398393
 
 
 // b value Conversion 
 
-console.log(typeof(b))
-console.log(Number(b))
-console.log(String(b))
+console.log(typeof(b)) // boolean
+console.log(Number(b)) // 1
+console.log(String(b)) // true
 
 
 // s value Conversion 
 
-console.log(typeof(s))
-console.log(Boolean(s))
-console.log(Number(s))
+console.log(typeof(s)) // String
+console.log(Boolean(s)) // true
+console.log(Number(s)) // NaN
 
 
 
 // sy value Conversion 
 
-console.log(typeof(sy))
-console.log(Boolean(sy))
-console.log(Number(sy))
-console.log(String(sy))
+console.log(typeof(sy)) // function 
+console.log(Boolean(sy)) // true
+console.log(Number(sy)) // NaN
+console.log(String(sy)) // function Symbol() { [native code] } 
 
 
 // N value Conversion 
 
-console.log(typeof(N))
-console.log(Boolean(N))
-console.log(Number(N))
-console.log(String(N))
+console.log(typeof(N)) // object
+console.log(Boolean(N)) // false
+console.log(Number(N)) // 0
+console.log(String(N)) // null
 
 
 
